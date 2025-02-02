7bfa8dce4a93958a2233e3e379b3b2e75f65f18a
usamaelzeibeady <usama.elzeibeady@gmail.com>
2025-02-02 21:37:39 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,12 @@
--- Display the full description of the 'books' table
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT, CHARACTER_MAXIMUM_LENGTH
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'books'
-  AND TABLE_SCHEMA = 'alx_book_store';
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    information_schema.columns
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
