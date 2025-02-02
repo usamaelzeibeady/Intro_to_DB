b7d98b554dfe3db8e79513da30d6ed81a2cf27e6
usamaelzeibeady <usama.elzeibeady@gmail.com>
2025-02-02 21:40:50 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,1 @@
-SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM 
-    information_schema.columns
-WHERE 
-    TABLE_SCHEMA = DATABASE() 
-    AND TABLE_NAME = 'books';
+SHOW COLUMNS FROM books;
